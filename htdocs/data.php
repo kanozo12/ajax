2333cb7dc90cbfe82605fabba8e98a8d0ab750f2
taejeong123 <auction4289@gmail.com>
2019-07-05 11:32:21 +0900

    20190705-ajax

---
+++ htdocs/data.php
@@ -0,0 +1,9 @@
+<?php
+
+$data = [
+    ['name'=>'gondr1', 'age'=>14, 'score'=>40],
+    ['name'=>'gondr2', 'age'=>14, 'score'=>40],
+    ['name'=>'gondr3', 'age'=>14, 'score'=>40],
+];
+
+echo json_encode($data);
